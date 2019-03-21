9d667bdb03f4c5568ad5aff67d413b8ac90f281d
dyike <yuanfeng634@gmail.com>
2019-03-21 20:47:45 +0800

    fix config

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -11,21 +13,11 @@ import (
 )
 
 var usageStr = `
-Usage: ontts [options]
-讯飞语音参数选项:
-    -tp <param>                 TTS合成参数[有默认值]
-    -lp <param>                 登录参数
+Usage: text2voice [options]
 单次合成模式选项:
     -t <text>                	待合成的文本
+    -p <text_file_path>         待合成的文本路径
     -o <file>               	音频输出路径 
-合成服务模式选项:
-    -d <dir>                    音频保存的目录 
-    -s <digit>                  合成速度级别(1-10),数值越小速度越快，越耗CPU[默认为1]
-    -r <addr>                   redis连接地址
-    -rp <pass>                  redis密码
-日志选项:
-    -l <file>                   日志输出路径[默认./ontts.log]
-    -ll <level>                 日志输出级别(debug,info,warn,error)
 其他:
     -h                          查看帮助 
 `
@@ -51,23 +43,61 @@ func configureLog(logFile, logLevel string) error {
 	return nil
 }
 
+type LogConf struct {
+	Enable   bool   `json:"enable"`
+	FileName string `json:"filename"`
+	Level    string `json:"level"`
+}
+
+type TTSConf struct {
+	VoiceName    string `json:"voice_name"`
+	TextEncoding string `json:"text_encoding"`
+	SampleRate   int    `json:"sample_rate"`
+	Speed        int    `json:"speed"`
+	Volume       int    `json:"volume"`
+	Pitch        int    `json:"pitch"`
+	Rdn          int    `json:"rdn"`
+}
+
+type Text2VoiceConfig struct {
+	Log     LogConf `json:"log"`
+	AppId   string  `json:"appid"`
+	WorkDir string  `json:"work_dir"`
+	Speed   int     `json:"speed"`
+	TTS     TTSConf `json:"tts"`
+}
+
+func LoadConfig(fileName string) (*Text2VoiceConfig, error) {
+	if len(fileName) == 0 {
+		return nil, errors.New("配置文件为空")
+	}
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	text2voiceConfig := &Text2VoiceConfig{}
+	err = decoder.Decode(text2voiceConfig)
+	if err != nil {
+		return nil, err
+	}
+	return text2voiceConfig, nil
+}
+
 func main() {
-	opts := &server.Options{}
 	var txt string
+	var txtPath string
 	var out string
 	var help bool
-	var logFile string
-	var logLevel string
-	flag.StringVar(&txt, "t", "", "单次合成的文本")
-	flag.StringVar(&out, "o", "", "单次合成的输出路径")
-	flag.StringVar(&logFile, "l", "voice.log", "日志输出路径")
-	flag.StringVar(&logLevel, "ll", "debug", "日志输出等级")
-	flag.BoolVar(&help, "h", false, "Help")
+	var confFile string
 
-	flag.StringVar(&opts.TTSParams, "tp", "voice_name = aisjiuxu, text_encoding = utf8, sample_rate = 16000, speed = 50, volume = 50, pitch = 50, rdn = 2", "TTS合成参数")
-	flag.StringVar(&opts.LoginParams, "lp", "appid = 5808ae7e, work_dir = .", "登录参数")
-	flag.StringVar(&opts.OutDir, "d", "", "音频输出目录")
-	flag.IntVar(&opts.Speed, "s", 1, "合成速度")
+	flag.StringVar(&confFile, "c", "default.json", "配置文件")
+	flag.StringVar(&txt, "t", "", "单次合成的文本")
+	flag.StringVar(&txtPath, "p", "", "合成文本文件路径")
+	flag.StringVar(&out, "o", "test.mp3", "单次合成的输出路径")
+	flag.BoolVar(&help, "h", false, "Help")
 
 	flag.Parse()
 
@@ -76,10 +106,37 @@ func main() {
 		return
 	}
 
-	err := configureLog(logFile, logLevel)
+	config, err := LoadConfig(confFile)
 	if err != nil {
-		log.Error("日志配置失败：%v", err)
+		fmt.Printf("加载配置文件失败:%v\n", err)
+		return
 	}
+
+	logLevel := config.Log.Level
+	logFile := config.Log.FileName
+
+	if config.Log.Enable {
+		err := configureLog(logFile, logLevel)
+		if err != nil {
+			log.Error("日志配置失败：%v", err)
+		}
+	}
+
+	opts := &server.Options{}
+	opts.TTSParams = fmt.Sprintf("voice_name = %s, text_encoding = %s, sample_rate= %d, speed = %d, volume = %d, pitch = %d, rdn = %d",
+		config.TTS.VoiceName,
+		config.TTS.TextEncoding,
+		config.TTS.SampleRate,
+		config.TTS.Speed,
+		config.TTS.Volume,
+		config.TTS.Pitch,
+		config.TTS.Rdn)
+
+	opts.LoginParams = fmt.Sprintf("appid = %s, work_dir = %s",
+		config.AppId,
+		config.WorkDir)
+
+	opts.Speed = config.Speed
 
 	s := server.New(opts)
 
@@ -89,7 +146,7 @@ func main() {
 		}
 		log.Debug("合成文本：%q，输出：%s", txt, out)
 
-		if err = s.Once(txt, out); err != nil {
+		if err := s.Once(txt, out); err != nil {
 			log.Error("%v", err)
 			return
 		}
